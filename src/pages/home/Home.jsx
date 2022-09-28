059aeb49ebe9611165c31ebe763a5f4f93e011c2
pragati suryawanshi <pragati.suryawanshi@yash.com>
2022-09-28 18:41:20 +0530

    added feturedlist, featured and propertilist component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Propertylist from "../../components/propertyList/Propertylist";
 
 import "./home.css";
 export const Home = () => {
@@ -8,6 +11,13 @@ export const Home = () => {
     <div>
      <Navbar></Navbar>
       <Header></Header>
+      <div className="homeContainer">
+        <Featured></Featured>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <Propertylist></Propertylist>
+        <h1 className="homeTitle">Homes guests love</h1>
+      <FeaturedProperties></FeaturedProperties>
+      </div>
     </div>
   );
 };
