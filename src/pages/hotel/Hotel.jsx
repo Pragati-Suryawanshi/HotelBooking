9cc76751470f2a4bd111b7df24812e0ae850cab9
pragati suryawanshi <pragati.suryawanshi@yash.com>
2022-09-29 17:55:24 +0530

    "Added Footer ,MaiLlist ,SerchItems and Hotel Component"

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+import './hotel.css'
 const Hotel = () => {
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar></Navbar>
+      <Header type="list"></Header>
+    </div>
   )
 }
 
